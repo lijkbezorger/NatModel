852028a50b3d6ab3864e3d43730acfb2b8208a51
Kirill Nazarov <knazarov@amcbridge.biz>
2015-11-18 23:27:30 +0200

    Grid drawing

--- NatModel/Form1.cs
+++ NatModel/Form1.cs
@@ -19,8 +19,27 @@ namespace NatModel
         {
             InitializeComponent();
 
-            field = new Field(20);
+            int size = 20;
+
+            field = new Field(size);
             field.Subscribe(this);
+
+            this.Width = size * 30 + 20;
+            this.Height = size * 30 + 40;
+
+            timer1.Tick += Timer1_Tick;
+            timer1.Enabled = true;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            field.Update();
+            Graphics g = CreateGraphics();
+            for (int i = 0; i <= field.Island.Length; i++)
+            {
+                g.DrawLine(Pens.Black, i * 30, 0, i * 30, field.Island.Length * 30);
+                g.DrawLine(Pens.Black, 0, i * 30, field.Island.Length * 30, i * 30);
+            }
         }
 
         public void Redraw()
